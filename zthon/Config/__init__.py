fc55516f1d30c62dc57d3c746b8cb642416bc307
tdebug163 <twqyqmhmd95@gmail.com>
2025-11-22 15:39:16 +0300

    Update __init__.py

--- zthon/Config/__init__.py
+++ zthon/Config/__init__.py
@@ -8,7 +8,7 @@ class Config:
     APP_ID = int(os.environ.get("APP_ID", 6))
     API_HASH = os.environ.get("API_HASH")
     ALIVE_NAME = os.environ.get("ALIVE_NAME")
-    DB_URI = os.environ.get("DB_URI")
+    DB_URI = os.environ.get("DATABASE_URL")
     STRING_SESSION = os.environ.get("STRING_SESSION")
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN")
     PRIVATE_GROUP_BOT_API_ID = int(os.environ.get("PRIVATE_GROUP_BOT_API_ID", -100))
