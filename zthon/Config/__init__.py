ffb05a91911005b9d7559db36c37cc8b4f6f27b3
tdebug163 <twqyqmhmd95@gmail.com>
2025-11-23 15:39:05 +0300

    Update __init__.py

--- zthon/Config/__init__.py
+++ zthon/Config/__init__.py
@@ -3,6 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Get the single, real OWNER_ID from Render
+# Make sure you have this variable set in Render's Environment!
+REAL_OWNER_ID = int(os.environ.get("OWNER_ID", 0))
+
 class Config:
     # Core Telegram Variables (from Render Environment)
     APP_ID = int(os.environ.get("APP_ID", 0))
@@ -13,24 +17,25 @@ class Config:
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN")
     PRIVATE_GROUP_BOT_API_ID = int(os.environ.get("PRIVATE_GROUP_BOT_API_ID", -100))
 
+    # --- The "Safety Net" for all possible owner variables ---
+    # We point all of them to the single REAL_OWNER_ID from Render
+    OWNER_ID = REAL_OWNER_ID
+    SUDO_USERS = [REAL_OWNER_ID]
+    DRAGONS = [REAL_OWNER_ID]
+    DEV_USERS = [REAL_OWNER_ID]
+    TIGERS = [REAL_OWNER_ID]
+    WOLVES = [REAL_OWNER_ID]
+    DEMONS = [REAL_OWNER_ID]
+
     # Variables to bypass Heroku-related errors
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", "FAKE_KEY_TO_BYPASS_HEROKU")
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", "FAKE_APP_NAME")
 
-    # The "Shield" - Common variables to prevent future errors
+    # Variables to fix import/startup errors
     COMMAND_HAND_LER = False
     GENIUS_API_TOKEN = os.environ.get("GENIUS_API_TOKEN", "FAKE_GENIUS_TOKEN")
     UPSTREAM_REPO = os.environ.get("UPSTREAM_REPO", "FAKE_UPSTREAM_REPO")
     PM_LOGGER_GROUP_ID = int(os.environ.get("PM_LOGGER_GROUP_ID", -100))
-    
-    # Add other common variables to be safe
-    LYDIA_API_KEY = os.environ.get("LYDIA_API_KEY", "FAKE_LYDIA_KEY")
-    SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID", "FAKE_SPOTIFY_ID")
-    SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET", "FAKE_SPOTIFY_SECRET")
-    REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", "FAKE_REMBG_KEY")
-    WALL_API = os.environ.get("WALL_API", "FAKE_WALL_API")
-    DEEPAI = os.environ.get("DEEPAI", "FAKE_DEEPAI")
-    TIME_ZONE = os.environ.get("TIME_ZONE", "Asia/Amman")
-OWNER_ID = int(os.environ.get("OWNER_ID", 0))
+
 class Zedub:
     pass
